d42450a5d549c09d63f23049de685396d83a8cf4
Yerko Ortiz <yerko.ortizmora@gmail.com>
2025-12-15 20:51:03 +0000

    feat: mappings for telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,3 +25,8 @@ require("lazy").setup({
     { import = "plugins.lsp" },
     { import = "plugins.editor" },
 })
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
